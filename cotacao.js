bea0e52ade19bb32d243b4337734d3a290d20841
rene-michel <53533335+rene-michel@users.noreply.github.com>
2022-05-11 18:56:12 -0300

    Add files via upload

--- cotacao.js
+++ cotacao.js
@@ -25,9 +25,32 @@ let u=new Usuario('rene','rene.michel@gmail.com', '123456', 1200)
 let date = new Date();
 console.log(date);
 
+function guarda_cotacao(){
+    if(dat == 0)
+        return;
+    var p = $.post("cotacao.php", {
+        'data_acesso' : Date.now(),
+        'ask' : dat['USDBRL']['ask'],
+        'bid' : dat['USDBRL']['bid'],
+        'high' : dat['USDBRL']['high'],
+        'low' : dat['USDBRL']['low'],
+        'pctChange' : dat['USDBRL']['pctChange'],
+        'varBid' : dat['USDBRL']['varBid'],
+        'code' : dat['USDBRL']['code'],
+        'codeIn' : dat['USDBRL']['codein'],
+        'create_date' : dat['USDBRL']['create_date'],
+        'timestamp' : dat['USDBRL']['timestamp'],
+        'name' : dat['USDBRL']['name']
+    });
+    p.done(function(d){
+        console.log(d);
+    });
+}
+
 setInterval(function(){
     obter_cot();
     console.log("Salario bruto " + u.salBr());
     console.log("Salario líquido " + u.liq());
     console.log(date);
+    guarda_cotacao();
 }, 60000);
